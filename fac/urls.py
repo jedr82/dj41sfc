0c53bfddf3c268ebdd8ef29454ea8b93cc27c797
Jose Diaz <jdiazrojas82@gmail.com>
2022-10-04 18:26:32 -0300

    modulo cliente new y edit agregados

--- fac/urls.py
+++ fac/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from fac.views import ClienteView
+from fac.views import ClienteView, ClienteNew, ClienteEdit
 
 app_name = 'fac_app'
 
 urlpatterns = [
     path('clientes/', ClienteView.as_view(), name='clientes_list'),
+    path('cliente/new', ClienteNew.as_view(), name='cliente_add'),
+    path('cliente/edit/<int:pk>', ClienteEdit.as_view(), name='cliente_edit'),
 ]
--- fac/views.py
+++ fac/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
 
 from bases.views import SinPrivilegios
 from fac.models import Cliente
 
+from .forms import ClienteForm
 
 # Create your views here.
 class ClienteView(SinPrivilegios, ListView):
@@ -10,3 +13,33 @@ class ClienteView(SinPrivilegios, ListView):
     template_name = 'fac/clientes_list.html'
     context_object_name = 'obj'
     permission_required = 'fac.view_cliente'
+
+class VistaBaseCreate(SuccessMessageMixin, SinPrivilegios, CreateView):
+    context_object_name = 'obj'
+    success_message = 'Registro agregado satisfactoriamente'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class VistaBaseEdit(SuccessMessageMixin, SinPrivilegios, UpdateView):
+    context_object_name = 'obj'
+    success_message = 'Registro actualizado satisfactoriamente'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+class ClienteNew(VistaBaseCreate):
+    model = Cliente
+    template_name = 'fac/cliente_form.html'
+    form_class = ClienteForm
+    success_url = reverse_lazy('fac_app:clientes_list')
+    permission_required = 'fac.add_cliente'
+
+class ClienteEdit(VistaBaseEdit):
+    model = Cliente
+    template_name = 'fac/cliente_form.html'
+    form_class = ClienteForm
+    success_url = reverse_lazy('fac_app:clientes_list')
+    permission_required = 'fac.change_cliente'
