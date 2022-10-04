52089c03cdbc4e03556edf13471b1bd0e60f0148
Jose Diaz <jdiazrojas82@gmail.com>
2022-10-04 19:30:57 -0300

    agregado funcion de inactivar cliente mediante ajax

--- fac/urls.py
+++ fac/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from fac.views import ClienteView, ClienteNew, ClienteEdit
+from fac.views import ClienteView, ClienteNew, ClienteEdit, inactivarCliente
 
 app_name = 'fac_app'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('clientes/', ClienteView.as_view(), name='clientes_list'),
     path('cliente/new', ClienteNew.as_view(), name='cliente_add'),
     path('cliente/edit/<int:pk>', ClienteEdit.as_view(), name='cliente_edit'),
+    path('cliente/estado/<int:id>', inactivarCliente, name='cliente_inactivar'),
 ]
--- fac/views.py
+++ fac/views.py
@@ -1,6 +1,8 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponse
 
 from bases.views import SinPrivilegios
 from fac.models import Cliente
@@ -43,3 +45,17 @@ class ClienteEdit(VistaBaseEdit):
     form_class = ClienteForm
     success_url = reverse_lazy('fac_app:clientes_list')
     permission_required = 'fac.change_cliente'
+
+
+@login_required(login_url="/login/")
+@permission_required("fac.change_cliente", login_url="/login/")
+def inactivarCliente(request, id):
+    cliente = Cliente.objects.filter(pk=id).first()
+
+    if request.method=='POST':
+        if cliente:
+            cliente.estado = not cliente.estado
+            cliente.save()
+            return HttpResponse("OK")
+        return HttpResponse("FAIL")
+    return HttpResponse("FAIL")
